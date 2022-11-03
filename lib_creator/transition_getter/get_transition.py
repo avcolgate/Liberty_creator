212837ceb6aca75a1b7ce31e2f65c8743b966203
avcolgate <avcolgate@gmail.com>
2022-11-03 18:00:39 +0300

    minor fix

--- lib_creator/transition_getter/get_transition.py
+++ lib_creator/transition_getter/get_transition.py
@@ -1,6 +1,7 @@
 import sys
 
 from transition_getter.classes import Template
+# from classes import Template
 
 file_path = sys.argv[1]
 
@@ -9,9 +10,13 @@ def get_net_transition(file_path):
     index_arr = []
     template_list = []
     is_template_section = False
+    lb_index = '("'
+    rb_index = '")'
 
 
     f = open(file_path, 'r')
+
+    # collecting body of all templates
     for line_num, line in enumerate(f):
         if 'lu_table_template' in line:
             is_template_section = True
@@ -21,9 +26,10 @@ def get_net_transition(file_path):
             if '}' in line:
                 is_template_section = False
                 template_list.append(template)
-        if 'timing()' in line:                      # temp!
+        if 'cell(' in line:                      # temp!
             break
 
+    # filling name, variables, indices of template
     for templ in template_list:
         for line in templ.body:
             if 'lu_table_template' in line:
@@ -33,11 +39,10 @@ def get_net_transition(file_path):
                 line = line[line.find(':')+1:line.find(';')].strip()
                 templ.variable.append(line)
             if 'index' in line:
-                lb = '("'
-                rb = '")'
-                line = line[line.find(lb)+len(lb):line.find(rb)].strip()
+                line = line[line.find(lb_index)+len(lb_index):line.find(rb_index)].strip()
                 templ.index.append(line)
 
+    # searching for right template
     for templ in template_list:
         if 'input_net_transition' in templ.variable and \
            'total_output_net_capacitance' in templ.variable:
