4c2c111a89eaa4a8a7cb97380746962217e95e62
avcolgate <avcolgate@gmail.com>
2022-11-24 14:50:04 +0300

    fix

--- lib_creator/get_trans.py
+++ lib_creator/get_trans.py
@@ -1,8 +1,8 @@
 import sys
-from transition_getter.main import get_net_transition
+import transition_getter.main as transition_getter
 
 liberty_path = sys.argv[1]
 
-transitions = get_net_transition(liberty_path) 
+transitions = transition_getter.get_net_transition(liberty_path) 
 
 print(transitions)
--- lib_creator/make_tcl.py
+++ lib_creator/make_tcl.py
@@ -1,8 +1,6 @@
 import sys
-import verilog_reader.main as verilog_parser
+import verilog_reader.main as verilog_reader
 import tcl_maker.main as tcl_maker
-
-# print(sys.argv)
 
 verilog_path = sys.argv[1]
 netlist_path = sys.argv[2]
@@ -12,6 +10,6 @@ tcl_dir  = sys.argv[5]
 out_dir = sys.argv[6]
 transitions = sys.argv[7]
 
-design_name, inputs = verilog_parser.get_module_inputs(verilog_path)
+design_name, inputs = verilog_reader.get_module_inputs(verilog_path)
 
 tcl_maker.make_tcl(design_name, inputs, clock_names, transitions, liberty_path, tcl_dir, out_dir, netlist_path)
