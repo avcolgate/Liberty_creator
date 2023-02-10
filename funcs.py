c7c2e6fbb66ee9afc224829bf8c9cb4bf6e6f0b6
avcolgate <avcolgate@gmail.com>
2023-02-10 17:08:26 +0300

    data_init() remake

--- funcs.py
+++ funcs.py
@@ -1,57 +1,81 @@
 import os
 from classes import Path
+from config import *
+from lib_creator.get_inputs.process_func import is_good_name
 
+def data_init() -> tuple[Path]:
 
-def data_init(
-        path_sta: str,
-        path_input_lib: str,
-        path_lef: str,
-        path_verilog: str,
-        path_netlist: str,
-        dir_results: str,
-        dir_tcl: str) -> tuple[Path]:
+    # design name and clock check
     try:
-        type_ = 'file'
-        name = 'OpenSTA'
-        path = path_sta
-        OPENSTA = Path(type_=type_, name=name, path=path)
-
-        type_ = 'file'
-        name = 'Input Library'
-        path = path_input_lib
-        LIBRARY = Path(type_=type_, name=name, path=path)
-
-        type_ = 'file'
-        name = 'LEF'
-        path = path_lef
-        LEF = Path(type_=type_, name=name, path=path)
-
-        type_ = 'file'
-        name = 'Verilog'
-        path = path_verilog
-        VERILOG = Path(type_=type_, name=name, path=path)
-
-        type_ = 'file'
-        name = 'Netlist'
-        path = path_netlist
-        NETLIST = Path(type_=type_, name=name, path=path)
-
-        type_ = 'directory'
-        name = 'Results'
-        path = dir_results
-        RESULTS = Path(type_=type_, name=name, path=path)
-
-        type_ = 'directory'
-        name = 'Tcl'
-        path = dir_tcl
-        TCL = Path(type_=type_, name=name, path=path)
-
+        if not is_good_name(design_name):
+            print('data init step:\n\tfatal: design_name is bad name!\n\texiting')
+            exit()
     except NameError:
-        print('data init step:\n\tfatal: %s %s is missing!\n\texiting' % (name, type_))
+        print('data init step:\n\tfatal: design_name is not defined in config.py!\n\texiting')
         exit()
-    else:
-        print('data init step:\n\tdata init success')
-        return OPENSTA, LIBRARY, LEF, VERILOG, NETLIST, RESULTS, TCL
+        
+    try:
+        for clk in clocks:
+            if not is_good_name(clk):
+                print('data init step:\n\tfatal: %s is bad clock name!\n\texiting' % clk)
+                exit()
+    except NameError:
+        print('data init step:\n\tfatal: clock list is not defined in config.py! set empty list if no clocks in design\n\texiting')
+        exit()
+
+    # paths check
+    try:
+        if not os.path.exists(path_opensta):
+            print('data init step:\n\tfatal: %s does not exist!\n\texiting' % path_opensta)
+            exit()
+    except NameError:
+        print('data init step:\n\tfatal: path_opensta is not defined in config.py!\n\texiting')
+        exit()
+
+    try:
+        if not os.path.exists(path_input_lib):
+            print('data init step:\n\tfatal: %s does not exist!\n\texiting' % path_input_lib)
+            exit()
+    except NameError:
+        print('data init step:\n\tfatal: path_input_lib is not defined in config.py!\n\texiting')
+        exit()
+
+    try:
+        if not os.path.exists(path_lef):
+            print('data init step:\n\tfatal: %s does not exist!\n\texiting' % path_lef)
+            exit()
+    except NameError:
+        print('data init step:\n\tfatal: path_lef is not defined in config.py!\n\texiting')
+        exit()
+
+    try:
+        if not os.path.exists(path_verilog):
+            print('data init step:\n\tfatal: %s does not exist!\n\texiting' % path_verilog)
+            exit()
+    except NameError:
+        print('data init step:\n\tfatal: path_verilog is not defined in config.py!\n\texiting')
+        exit()
+    
+    try:
+        if not os.path.exists(path_netlist):
+            print('data init step:\n\tfatal: %s does not exist!\n\texiting' % path_netlist)
+            exit()
+    except NameError:
+        print('data init step:\n\tfatal: path_netlist is not defined in config.py!\n\texiting')
+        exit()
+
+    # directories check
+    try:
+        clean_up_or_make(dir_results)
+    except NameError:
+        print('data init step:\n\tfatal: dir_results is not defined in config.py!\n\texiting')
+        exit()
+
+    try:
+        clean_up_or_make(dir_tcl)
+    except NameError:
+        print('data init step:\n\tfatal: dir_tcl is not defined in config.py!\n\texiting')
+        exit()
 
 
 def clean_up_or_make(dir_name: str) -> None:
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 
-from funcs import clean_up_or_make, data_init
+from funcs import data_init
 from config import *
 
 from lib_creator.get_inputs.main import get_module_name, get_module_inputs
@@ -10,22 +10,18 @@ from lib_creator.parse_templates.main import get_transitions
 from lib_creator.make_tcl.main import make_tcl
 from lib_merger.merger.main import merge_lib
 
-OPENSTA, LIBRARY, LEF, VERILOG, NETLIST, RESULTS, TCL = data_init(path_opensta, path_input_lib, path_lef,
-                                                                  path_verilog, path_netlist, dir_results, dir_tcl)
+data_init()
 
-clean_up_or_make(RESULTS.path)
-clean_up_or_make(TCL.path)
+module_name = get_module_name(path_verilog)
+module_inputs = get_module_inputs(path_verilog)
+size = get_size(path_lef)
+pin_transitions = get_transitions(path_input_lib)
 
-module_name = get_module_name(VERILOG.path)
-module_inputs = get_module_inputs(VERILOG.path)
-size = get_size(LEF.path)
-pin_transitions = get_transitions(LIBRARY.path)
-
-make_tcl(module_name, module_inputs, clocks, pin_transitions, LIBRARY.path, TCL.path, RESULTS.path, NETLIST.path)
+make_tcl(module_name, module_inputs, clocks, pin_transitions, path_input_lib, dir_tcl, dir_results, path_netlist)
 
 clk_transition = 'NaN' if not clocks else pin_transitions
 
-temp_tcl_path = TCL.path + '/temp.tcl'
+temp_tcl_path = dir_tcl + '/temp.tcl'
 
 for clk_t in clk_transition:
     for pin_t in pin_transitions:
@@ -33,17 +29,17 @@ for clk_t in clk_transition:
             text = file.read()
             text = text.replace('%pin%', str(pin_t))
             text = text.replace('%clk%', str(clk_t))
-            exec_path = TCL.path + '/make_lib_clk_%s_pin_%s.tcl' % (clk_t, pin_t)
+            exec_path = dir_tcl + '/make_lib_clk_%s_pin_%s.tcl' % (clk_t, pin_t)
             with open(file=exec_path, mode="w") as file_out:
                 file_out.write(text)
 
-            cmd = OPENSTA.path + ' -no_splash ' + exec_path
+            cmd = path_opensta + ' -no_splash ' + exec_path
             os.system(cmd)
             os.remove(exec_path)
 
             print('ready: clk_%s_pin_%s' % (clk_t, pin_t))
 os.remove(temp_tcl_path)
 
-module_leakage = get_leakage("%s/%s_power.txt" % (RESULTS.path, module_name))
+module_leakage = get_leakage("%s/%s_power.txt" % (dir_results, module_name))
 
-merge_lib(RESULTS.path)
+merge_lib(dir_results)
