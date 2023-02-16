8be22ca5659a23f53d60985d9f5d0fae4677bf29
avcolgate <avcolgate@gmail.com>
2023-02-16 14:10:52 +0300

    minor fix

--- config.py
+++ config.py
@@ -8,10 +8,3 @@ path_input_lib = 'tests/data/source/liberty/nom_1.65V_25C/CORELIB8DLL.lib'
 path_lef = 'tests/data/source/lib_sample/lib_sample.lef'
 
 dir_results = 'tests/data/results/lib_sample'
-
-
-# path_lef = 'tests/data/source/%s/%s.lef' % (design_name, design_name)
-# path_verilog = 'tests/data/source/%s/%s.v' % (design_name, design_name)
-# path_netlist = 'tests/data/source/%s/%s_netlist_mikron.v' % (design_name, design_name)
-
-# dir_results = 'tests/data/results/%s' % design_name
